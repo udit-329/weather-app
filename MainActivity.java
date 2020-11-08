32681a8b647045fac1734667d7ab52c32648a0d8
Tess Gompper <33359970+tgompper1@users.noreply.github.com>
2020-11-08 09:39:44 -0500

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,8 +2,13 @@ package com.example.shine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.telecom.Call;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -15,23 +20,22 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
+    //private static String OPEN_WEATHER_MAP_URL = "api.openweathermap.org/data/2.5/weather?q=Toronto&appid=";
+    //private static String OPEN_WEATHER_MAP_API = "542ce61fca8f88351ade3116575cd0e8";
     TextView currentTemp, city, currentDescription, date, bestWeatherTemp, bestWeatherDescription, bestTime;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)   {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //currentTemp = (TextView)findViewById(R.id.currentTempField);
-        //currentDescription = (TextView)findViewById(R.id.currentDescriptionField);
+        currentTemp = (TextView)findViewById(R.id.currentTempField);
+        currentDescription = (TextView)findViewById(R.id.currentDescriptionField);
         //city = (TextView)findViewById(R.id.cityField);
-        //date = (TextView)findViewById(R.id.dateField);
+        date = (TextView)findViewById(R.id.dateField);
         //find_current_weather();
 
         bestWeatherTemp = (TextView)findViewById(R.id.bestTempField);
@@ -39,16 +43,63 @@ public class MainActivity extends AppCompatActivity {
         bestTime = (TextView)findViewById(R.id.bestTimeField);
         find_best_weather();
     }
+
+    public void find_current_weather(){
+        date.setText("tess");
+        //URL url1 = new URL
+        String url = "http://api.openweathermap.org/data/2.5/weather?q=Toronto&appid=542ce61fca8f88351ade3116575cd0e8&units=Metric";
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        date.setText("hi");
+                       try {
+                           date.setText("hi");
+                           JSONObject main_object = response.getJSONObject("main");
+                           JSONArray array = response.getJSONArray("weather");
+                           JSONObject object = array.getJSONObject(0);
+                           String temp = String.valueOf(main_object.getDouble("temp"));
+                           String description = object.getString("description");
+                           String city = response.getString("name");
+
+                           currentTemp.setText(temp);
+                           currentDescription.setText(description);
+                       }
+                        catch(JSONException e){
+                        date.setText("bye");
+                        e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        date.setText("hello");
+                    }
+                });
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(jsonObjectRequest);
+    }
+
+
+
+
     protected void find_best_weather(){
-        double[] scores = new double[13]; //sized based on hour range index represent hour
-        double[] temps = {20, 18, 15, 25, 17}; //TODO populate with real temps
-        int[] descriptions = {800, 600,602,803,801}; //TODO populate with real description id
+        double[] scores = new double[20]; //sized based on hour range index represent hour
+        //toronto
+        //double[] temps = {11, 12, 13, 14, 15, 14, 15.5, 15.5, 15, 14, 14, 12, 12}; //TODO populate with real temps
+        //int[] descriptions = {800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800, 800}; //TODO populate with real description id
+        //calgary
+        double[]temps = {-9, -9, -8, -7, -7, -6, -6, -5.5, -5.5, -5.5, -6, -6, -7};
+        int[] descriptions = { 602, 602, 802, 802, 802, 802, 802, 804, 802, 801, 802, 801, 801};
+
         for(int i = 0; i < temps.length-1;i++){
             scores[i] = temps[i];
         }
         for(int i = 0; i < descriptions.length-1;i++){
             if (descriptions[i] == 800) { scores[i] += 13;}
-            else if(descriptions[i] == 801 || descriptions[i] == 801){scores[i]+=12;}
+            else if(descriptions[i] == 801 || descriptions[i] == 802){scores[i]+=12;}
             else if(descriptions[i]== 803 || descriptions[i] == 804 ){scores[i]+=11;}
             else if(descriptions[i] == 600 || descriptions[i] == 601){scores[i]+= 10;}
             else if(descriptions[i] == 701 || descriptions[i] == 741){scores[i]+=9;}
@@ -69,22 +120,19 @@ public class MainActivity extends AppCompatActivity {
                 highestScoreIndex = i;
             }
         }
+        bestTime.setText(getTime(highestScoreIndex));
         bestWeatherTemp.setText(String.valueOf(temps[highestScoreIndex]));
         bestWeatherDescription.setText(getDescription(descriptions[highestScoreIndex]));
-        bestTime.setText(getTime(highestScoreIndex));
     }
+
     protected String getTime(int index){
         index = index+8;
-        int hourLater = index+1;
-        if(index < 11){
-            return "between " + index + "AM and " + hourLater + "AM";
-        }
-        if(index == 11){
-            return "between 11am and 12PM";
+        if(index < 12){
+            return index + ":30 AM ";
         }
         else{
             index -= 12;
-            return "between " + index + "PM and " + hourLater + "PM";
+            return index + ":30 PM ";
         }
     }
 
@@ -137,49 +185,6 @@ public class MainActivity extends AppCompatActivity {
         else return null;
     }
 
-    protected void find_current_weather(){
-        String url = "http://api.openweathermap.org/data/2.5/weather?q=toronto&appid=542ce61fca8f88351ade3116575cd0e8&units=metric";
 
-        JsonObjectRequest jor = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
 
-                try {
-                    date.setText("hi");
-                    JSONObject main_object = response.getJSONObject("main");
-                    JSONArray array = response.getJSONArray("weather");
-                    JSONObject object = array.getJSONObject(0);
-                    String temp = String.valueOf(main_object.getDouble("temp"));
-                    String description = object.getString("description");
-                    String city1 = response.getString("name");
-
-                    currentTemp.setText(temp);
-                    currentDescription.setText(description);
-                    city.setText(city1);
-
-                   // Calendar calendar = new Calendar.getInstance());
-                    //SimpleDateFormat sdf = new SimpleDateFormat("EEEE-MM-dd");
-                   // String formatted_date = sdf.format(calendar.getTime());
-
-                    //date.setText(formatted_date);
-
-                    double temp_int = Double.parseDouble(temp);
-                    int i = (int)temp_int;
-                    currentTemp.setText(String.valueOf(i));
-                }
-                catch(JSONException e){
-                    //date.setText("bye");
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-            //date.setText("no");
-            }
-        });
-        //date.setText("ugh");
-        RequestQueue queue = Volley.newRequestQueue(this);
-        queue.add(jor);
-    }
 }
